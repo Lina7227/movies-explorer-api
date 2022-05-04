8ccce1a87c339f2777e9a823290a4bc5892ab490
unknown <elena.crivoscheina@yandex.ru>
2022-05-04 19:37:25 +0300

    переустановка cors

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 const corsOptions = [
   'https://diploma-gallery-movies.herokuapp.com',
-  'https://movies-explorer-frontend-black.vercel.app',
+  'https://movies-explorer-frontend-black.vercel.app/',
   'http://localhost:3001',
   'http://localhost:3000',
   'https://web.postman.co',
